89ddf427d39ccc9ee17bba093a8202b4f509decd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-10-28 20:03:04 +0530

    OreForge: base project

---
+++ OreForge/OreForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OreForgeApp.swift
+//  OreForge
+//
+//  Created by Krisna Pranav on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct OreForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
